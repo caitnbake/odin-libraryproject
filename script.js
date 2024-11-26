30cb57d073fb07df049a8a08dfc33c2e82eef3bb
caitnbake <caitlin.atterberry@gmail.com>
2024-11-25 22:39:19 -0800

    Add addBooksToPage function

--- script.js
+++ script.js
@@ -15,9 +15,32 @@ function addBookToLibrary(title, author, numberOfPages, haveRead) {
     myLibrary.push(newBook);
 }
 
+let container = document.querySelector(".container");
+
+function addBooksToPage() {
+    for (book of myLibrary) {
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+        const bookTitle = document.createElement("h2");
+        bookTitle.classList.add("bookTitle");
+        bookTitle.innerText = book.title;
+        const bookAuthor = document.createElement("h3");
+        bookAuthor.classList.add("bookAuthor");
+        bookAuthor.innerText = book.author;
+        const bookPages = document.createElement("p");
+        bookPages.classList.add("numberOfPages");
+        bookPages.innerText = `Number of pages: ${book.numberOfPages}`;
+        const haveRead = document.createElement("checkbox");
+        haveRead.innerText = `Read: ${book.haveRead}`;
+        newCard.appendChild(bookTitle);
+        newCard.appendChild(bookAuthor);
+        newCard.appendChild(bookPages);
+        newCard.appendChild(haveRead);
+        container.appendChild(newCard);
+    }
+}
+
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 295, 'no');
 addBookToLibrary('Made Up Book', 'Fake Author', 893, 'yes');
 addBookToLibrary('Fake Book 2', 'Cait Baker', 86, 'yes');
-
-console.table(myLibrary);
-console.log(myLibrary[0].info());
+addBooksToPage();
