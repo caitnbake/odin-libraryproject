2281a2961a6bb4129540b4e7b35a3d566121dda8
caitnbake <caitlin.atterberry@gmail.com>
2024-11-27 16:49:51 -0800

    Add popup modal

--- script.js
+++ script.js
@@ -1,55 +1,77 @@
 const myLibrary = [];
 
+let selector = {
+    container: document.querySelector(".container"),
+    newBtn: document.querySelector(".addBook"),
+    stats: document.querySelector(".stats"),
+    popup: document.querySelector(".popup"),
+    cancelBtn: document.querySelector(".cancel"),
+}
+
+selector.newBtn.addEventListener("click", () => {
+    selector.popup.showModal();
+});
+
+selector.cancelBtn.addEventListener("click", () => {
+    selector.popup.close();
+});
+
 function Book(title, author, numberOfPages, status) {
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.status = status;
+    this.isNew = true;
 }
 
 function addBookToLibrary(title, author, numberOfPages, status) {
     const newBook = new Book(title, author, numberOfPages, status);
     myLibrary.push(newBook);
+    addBooksToPage();
 }
 
-let container = document.querySelector(".container");
-
 function addBooksToPage() {
-    const totalBooks = document.querySelector(".total");
-    totalBooks.innerHTML = `<span class="strong">Total:</span> ${myLibrary.length}`;
-
-    const readBooks = document.querySelector(".read");
-    const totRead = myLibrary.filter(book => book.status === "Finished");
-    readBooks.innerHTML = `<span class="strong">Read:</span> ${totRead.length}`;
-
-    const inProgress = document.querySelector(".inprogress");
-    const totInProgress = myLibrary.filter(book => book.status === "Started");
-    inProgress.innerHTML = `<span class="strong">In progress:</span> ${totInProgress.length}`;
-
-    const notStarted = document.querySelector(".notstarted");
-    const totNotStarted = myLibrary.filter(book => book.status === "Not started");
-    notStarted.innerHTML = `<span class="strong">Not started:</span> ${totNotStarted.length}`;
-
     for (book of myLibrary) {
-        const newCard = document.createElement("div");
-        newCard.classList.add("card");
-        const bookTitle = document.createElement("h2");
-        bookTitle.classList.add("bookTitle");
-        bookTitle.innerText = book.title;
-        const bookAuthor = document.createElement("p");
-        bookAuthor.classList.add("bookAuthor");
-        bookAuthor.innerHTML = `<span class="strong white">Author:</span> ${book.author}`;
-        const bookPages = document.createElement("p");
-        bookPages.classList.add("numberOfPages");
-        bookPages.innerHTML = `<span class="strong white">Number of pages:</span> ${book.numberOfPages}`;
-        const haveRead = document.createElement("p");
-        haveRead.innerHTML = `<span class="strong white">Status:</span> ${book.status}`;
-        newCard.appendChild(bookTitle);
-        newCard.appendChild(bookAuthor);
-        newCard.appendChild(bookPages);
-        newCard.appendChild(haveRead);
-        container.appendChild(newCard);
+        if (book.isNew === true) {
+            let newElement = {
+                card: document.createElement("div"),
+                bookTitle: document.createElement("h2"),
+                bookAuthor: document.createElement("p"),
+                bookPages: document.createElement("p"),
+                haveRead: document.createElement("p"),
+            }
+
+            newElement.card.classList.add("card");
+            newElement.bookTitle.classList.add("bookTitle");
+            newElement.bookAuthor.classList.add("bookAuthor");
+
+            newElement.bookTitle.innerText = book.title;
+            newElement.bookAuthor.innerHTML = `<span class="strong white">Author:</span> ${book.author}`;
+            newElement.bookPages.innerHTML = `<span class="strong white">Number of pages:</span> ${book.numberOfPages}`;
+            newElement.haveRead.innerHTML = `<span class="strong white">Status:</span> ${book.status}`;
+
+            newElement.card.appendChild(newElement.bookTitle);
+            newElement.card.appendChild(newElement.bookAuthor);
+            newElement.card.appendChild(newElement.bookPages);
+            newElement.card.appendChild(newElement.haveRead);
+            selector.container.appendChild(newElement.card);
+            book.isNew = false;
+        }
     }
+    updateStats();
+}
+
+function updateStats() {
+     let stats = {
+        totRead: myLibrary.filter(book => book.status === "Finished"),
+        totInProgress: myLibrary.filter(book => book.status === "Started"),
+        totNotStarted: myLibrary.filter(book => book.status === "Not started"),
+    }
+
+    selector.stats.innerHTML = `<p><span class="strong">Read:</span> ${stats.totRead.length}</p>
+    <p><span class="strong">In progress:</span> ${stats.totInProgress.length}</p>
+    <p><span class="strong">Not started:</span> ${stats.totNotStarted.length}</p>
+    <p><span class="strong">Total:</span> ${myLibrary.length}</p>`;
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 295, 'Started');
@@ -58,4 +80,3 @@ addBookToLibrary('Fake Book 2', 'Cait Baker', 86, 'Finished');
 addBookToLibrary('Long Book Title That Takes', 'J.R.R. Tolkein', 295, 'Not started');
 addBookToLibrary('Made Up Book', 'Fake Author', 893, 'Started');
 addBookToLibrary('Fake Book 2', 'Cait Baker', 86, 'Not started');
-addBooksToPage();
